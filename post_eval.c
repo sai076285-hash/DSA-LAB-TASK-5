5efd38088b97f338dbb2055cfba164b546ca084c
sai076285-hash <sai076285@gmail.com>
2026-02-23 16:09:57 +0530

    Implement postfix expression evaluator in C

---
+++ post_eval.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int x) { stack[++top] = x; }
+int pop() { return stack[top--]; }
+
+int main() {
+    char postfix[MAX];
+    int i, a, b;
+
+    printf("Enter Postfix Expression: ");
+    scanf("%s", postfix);
+
+    for(i = 0; postfix[i] != '\0'; i++) {
+
+        if(isdigit(postfix[i]))
+            push(postfix[i] - '0');
+
+        else {
+            b = pop();
+            a = pop();
+
+            switch(postfix[i]) {
+                case '+': push(a + b); break;
+                case '-': push(a - b); break;
+                case '*': push(a * b); break;
+                case '/': push(a / b); break;
+            }
+        }
+    }
+
+    printf("Result = %d", pop());
+    return 0;
+}
